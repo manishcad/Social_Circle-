7cb275e85286ba111df5de040ec3eea706a3754d
Manish Chand <manishtochand@gmail.com>
2025-07-05 20:50:00 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import Layout from "./components/Layout";
-
+import { Suspense } from "react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,11 +24,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          <Layout>
-            {children}
-          </Layout>
-        </Providers>
+        <Suspense fallback={<p>Loading</p>}>
+          <Providers>
+            <Layout>
+              {children}
+            </Layout>
+          </Providers>
+        </Suspense>
       </body>
     </html>
   );
